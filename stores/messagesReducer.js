c283c4f78454de3f84445fccc71d8cb89fa456a8
Morgan <Morgantheplant@gmail.com>
2016-04-09 09:50:52 -0700

    Update messagesReducer.js

--- stores/messagesReducer.js
+++ stores/messagesReducer.js
@@ -1,9 +1,15 @@
-export const messagesStore = (state ={ messages:[] }, action) => {
+export const messagesStore = (state ={ messages:[], showPanel:true }, action) => {
     switch (action.type){    
         case 'ADD_ROOM_MESSAGE':
           return {
-            messages: [action.room_message].concat(state.messages)
-          }          
+            messages: [action.room_message].concat(state.messages),
+            showPanel: state.showPanel
+          }  
+       case: 'TOGGLE_MESSAGE_PANE':   
+          return {
+            messages: state.messages,
+            showPanel: !state.showPanel
+          }     
      default:
         return state;    
     }
